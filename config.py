e4dd4c6661081af7f72688633c2225a696f7d877
agu-am <agustin.cs96@gmail.com>
2025-06-11 02:17:05 -0300

    primer deploy

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-TOKEN_BOT = "7028275899:AAFqENbtMg1OZlhpSrMI32iLik3LlSPS098"
+TOKEN_BOT = "7746530165:AAE_wNkQOy8hni_vVoeHTi16bQkYW2vc5uA"
 ID_ADMIN = 1108656550 # Reemplaza con tu ID numérica de usuario de Telegram
 ID_CANAL_FOTOS = -1002738063463# Reemplaza con el ID numérico de tu canal (asegúrate de incluir el -100 si lo tiene)
